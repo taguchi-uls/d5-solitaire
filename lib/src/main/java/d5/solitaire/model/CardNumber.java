5ee3b2c3e56c4818b079c945c81d9dd2ad8f773e
tomoyuki-nagata <tomolog925@gmail.com>
2022-06-18 15:25:43 +0900

    toStringとequalsとhashCodeの実装をLombokに変更

--- lib/src/main/java/d5/solitaire/model/CardNumber.java
+++ lib/src/main/java/d5/solitaire/model/CardNumber.java
@@ -1,11 +1,13 @@
 package d5.solitaire.model;
 
-import java.util.Objects;
-
 import javax.annotation.Nonnull;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.ToString;
 
+@ToString
+@EqualsAndHashCode
 public class CardNumber {
 	private final Integer MIN_VALUE = 1;
 	private final Integer MAX_VALUE = 13;
@@ -23,27 +25,5 @@ public class CardNumber {
 	@Nonnull
 	public static CardNumber of(@Nonnull Integer value) {
 		return new CardNumber(value);
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(value);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		CardNumber other = (CardNumber) obj;
-		return Objects.equals(value, other.value);
-	}
-
-	@Override
-	public String toString() {
-		return "CardNumber [value=" + value + "]";
 	}
 }
