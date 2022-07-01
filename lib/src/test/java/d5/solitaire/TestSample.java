ea1b5c5ce1a5633cf328b902cc0a8e25fc5f86c0
taguchi-uls <ryotaro.taguchi@ulsystems.co.jp>
2022-07-01 17:52:52 +0900

    JUnit5を導入

---
+++ lib/src/test/java/d5/solitaire/TestSample.java
@@ -0,0 +1,12 @@
+package d5.solitaire;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestSample {
+
+    @Test
+    public void test() {
+        Assertions.assertTrue(true);
+    }
+}
